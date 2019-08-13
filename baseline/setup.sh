cd9133366bee68f5d40cbb1937909e5b2018fda8
Sascha Egerer <sascha-egerer@users.noreply.github.com>
2019-08-13 11:53:04 +0200

    Install required php7-zlib package

--- baseline/setup.sh
+++ baseline/setup.sh
@@ -6,7 +6,7 @@ zypper --non-interactive modifyrepo --enable repo-update
 zypper --non-interactive ref
 zypper --non-interactive update
 # Install requirements
-zypper --gpg-auto-import-keys --non-interactive in --force-resolution nginx php7-fpm php7-mbstring php7-mysql php7-curl php7-pcntl php7-gd php7-openssl php7-ldap php7-fileinfo php7-posix php7-json php7-iconv php7-ctype php7-zip php7-sockets which python3-Pygments nodejs ca-certificates ca-certificates-mozilla ca-certificates-cacert sudo subversion mercurial php7-dom php7-xmlwriter php7-opcache ImageMagick postfix glibc-locale git python-pip npm8 hostname mariadb-client vim vim-data cronie
+zypper --gpg-auto-import-keys --non-interactive in --force-resolution nginx php7-fpm php7-mbstring php7-mysql php7-curl php7-pcntl php7-gd php7-openssl php7-ldap php7-fileinfo php7-posix php7-json php7-iconv php7-ctype php7-zip php7-sockets php7-zlib which python3-Pygments nodejs ca-certificates ca-certificates-mozilla ca-certificates-cacert sudo subversion mercurial php7-dom php7-xmlwriter php7-opcache ImageMagick postfix glibc-locale git python-pip npm8 hostname mariadb-client vim vim-data cronie
 
 npm install -g ws
 pip install supervisor
